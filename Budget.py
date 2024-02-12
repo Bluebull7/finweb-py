75c6a78c8425e77378b95e5c94b3bae0ba1cd4dd
Lance H <lsaucyfx@gmail.com>
2024-02-12 13:58:33 -0500

    created budget class

---
+++ Budget.py
@@ -0,0 +1,25 @@
+class Budget(object):
+
+    def __init__(self, budgetId: int, amountAllocated: float):
+        
+        self.budgetId = budgetId
+        self.amountAllocated = amountAllocated
+
+    def getRemainingAmount() -> float:
+
+        # <TODO> 
+        # IMPLEMENT LOGIC 
+        pass
+
+    def addExpense(expense: Expense):
+        # <TODO>
+        # IMPLEMENT LOGIC
+        # CREATE EXPENSE CLASS TO DEFINE TYPE EXPENSE
+        pass
+
+    def getBudgetDetails() -> str:
+        # <TODO>
+        # IMPLEMENT LOGIC
+        pass
+
+
